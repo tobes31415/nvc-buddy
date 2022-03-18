a074718d90c0972ce8320964663b33fd83a28e64
Jake Tober <jake@macadamian.com>
2022-03-18 13:18:10 -0400

    Initial Code Commit

---
+++ src/services/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBwFaxsELgtW236HL88AZpPSzAq34B1wu0",
+  authDomain: "nvc-buddy.firebaseapp.com",
+  projectId: "nvc-buddy",
+  storageBucket: "nvc-buddy.appspot.com",
+  messagingSenderId: "661782363299",
+  appId: "1:661782363299:web:54edbf5e4dd865cfb1d316",
+  measurementId: "G-Z6D37M29K7",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
---
+++ src/test.ts
@@ -0,0 +1,7 @@
+console.log("hello world");
+
+const abc = 123;
+
+if (abc === 123) {
+  console.log("123");
+}
